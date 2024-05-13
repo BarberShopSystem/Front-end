c8baf15af23f818824184f488282544f30bb5aee
Pedro-Henrique-Costa <pedrohcosta0209@gmail.com>
2024-05-13 18:50:35 -0300

    feat: create menu mobile

---
+++ src/shared/components/MenuMobile/MenuMobile.tsx
@@ -0,0 +1,8 @@
+import { Drawer } from '@mui/material'
+import './MenuMobile.css'
+
+export const MenuMobile: React.FC = () => {
+    return (
+        <Drawer variant={'temporary'}>Teste</Drawer>
+    )
+}
--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -1,22 +1,35 @@
+import { useState } from 'react';
 import './Navbar.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((open) => !open)
+  }
+
   return (
     <header>
-      <section className='navbar'>
 
-        <div>
-          <img className='logo' src="../../public/img/Component 17.png" alt="" />
-          <h5 className='darkbrown-text'>ESTAMOS ABERTOS</h5>
-          <h5 className='whitebrown-text'>7 DIAS NA SEMANA</h5>
+      <section className={`navbar ${isOpen ? "is-open" : ""}`}>
+
+        <div className={`hide ${isOpen ? "is-open" : ""}`}>
+          <img className={`logo ${isOpen ? "is-open" : ""}`} src="../../public/img/Component 17.png" alt="" />
+          <h5 className={`darkbrown-text ${isOpen ? "is-open" : ""}`}>ESTAMOS ABERTOS</h5>
+          <h5 className={`whitebrown-text ${isOpen ? "is-open" : ""}`}>7 DIAS NA SEMANA</h5>
         </div>
+        
+        <aside className={`aside ${isOpen ? "is-open" : ""}`}>
+          <img className="hide" src="../../public/img/Union.svg" alt="" />
+          <h5 className={`whitebrown-text ${isOpen ? "is-open" : ""}`}>ENDEREÇO</h5>
+          <img className="hide" src="../../public/img/Vector.svg" alt="" />
+          <h5 className={`whitebrown-text ${isOpen ? "is-open" : ""}`}>(00)00000-0000</h5>
+          <button className={`hide ${isOpen ? "is-open" : ""}`} >AGENDAR</button>
 
-        <aside>
-          <img src="../../public/img/Union.svg" alt="" />
-          <h5 className='whitebrown-text'>ENDEREÇO</h5>
-          <img src="../../public/img/Vector.svg" alt="" />
-          <h5 className="whitebrown-text">(00)00000-0000</h5>
-          <button>AGENDAR</button>
+          <MenuIcon fontSize='large' className='menu-mobile' onClick={toggleMenu}/>
+          
         </aside>
 
       </section>
