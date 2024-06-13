cb39d6661fa3902570f64ad0cc3f7cb49702022b
Limael <limagustavorafael@hotmail.com>
2024-06-13 20:49:12 -0300

    Made styled modal

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
+import { ptBR } from 'date-fns/locale/pt-BR';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
+      <App />
+    </LocalizationProvider>
   </React.StrictMode>,
 )
--- src/shared/components/ServiceModal/ServiceModal.tsx
+++ src/shared/components/ServiceModal/ServiceModal.tsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
-import DatePicker from 'react-datepicker';
+import {
+  Modal,
+  Box,
+  Typography,
+  TextField,
+  MenuItem,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  Grid,
+  Alert,
+  InputAdornment
+} from '@mui/material';
 import 'react-datepicker/dist/react-datepicker.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './ServiceModal.css';
+import './ServiceModal.css'; // Se houver estilos personalizados
 
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { DatePicker } from '@mui/x-date-pickers';
 type Profissional = {
   id: number;
   nome: string;
@@ -17,7 +30,8 @@ type ModalProps = {
 };
 
 const ServicoModal: React.FC<ModalProps> = ({ show, handleClose, profissionais }) => {
-  const [selectedProfissional, setSelectedProfissional] = useState<number | null>(null);
+  const [activeStep, setActiveStep] = useState<number>(0);
+  const [selectedProfissional, setSelectedProfissional] = useState<string>('');
   const [horario, setHorario] = useState<string>('');
   const [regiao, setRegiao] = useState<string>('');
   const [data, setData] = useState<Date | null>(new Date());
@@ -25,12 +39,19 @@ const ServicoModal: React.FC<ModalProps> = ({ show, handleClose, profissionais }
   const [nome, setNome] = useState<string>('');
   const [telefone, setTelefone] = useState<string>('');
   const [formSubmitted, setFormSubmitted] = useState<boolean>(false);
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (validateForm()) {
       const formData = {
-        selectedProfissional,
+        selectedProfissional: Number(selectedProfissional),
         horario,
         regiao,
         data,
@@ -69,67 +90,330 @@ const ServicoModal: React.FC<ModalProps> = ({ show, handleClose, profissionais }
   };
 
   return (
-    <Modal show={show} onHide={handleClose} className="modal-custom">
-      <Modal.Header closeButton>
-        <Modal.Title>Agendar Serviço</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
+    <Modal open={show} onClose={handleClose}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '60%',
+          bgcolor: 'background.paper',
+          boxShadow: 24,
+          p: 4,
+          borderRadius: 2
+        }}
+      >
+        <Typography sx={{ color: '#CAB392', fontWeight: 800, fontSize: 38 }} variant="h5" component="h2" align="center" gutterBottom>
+          FAÇA SEU AGENDAMENTO
+        </Typography>
         {formSubmitted ? (
-          <div className="alert alert-success" role="alert">
-            Serviço agendado com sucesso!
-          </div>
+          <Alert severity="success">Serviço agendado com sucesso!</Alert>
         ) : (
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formProfissional">
-              <Form.Label>Selecionar Profissional</Form.Label>
-              <Form.Control
-                as="select"
-                value={selectedProfissional || ''}
-                onChange={e => setSelectedProfissional(Number(e.target.value))}
-                required
-              >
-                <option value="" disabled>Escolha um profissional</option>
-                {profissionais.map(prof => (
-                  <option key={prof.id} value={prof.id}>{prof.nome}</option>
-                ))}
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="formHorario">
-              <Form.Label>Horário</Form.Label>
-              <Form.Control type="time" value={horario} onChange={e => setHorario(e.target.value)} required />
-            </Form.Group>
-            <Form.Group controlId="formRegiao">
-              <Form.Label>Região do Serviço</Form.Label>
-              <Form.Control type="text" value={regiao} onChange={e => setRegiao(e.target.value)} required />
-            </Form.Group>
-            <Form.Group controlId="formData">
-              <Form.Label>Data</Form.Label>
-              <DatePicker selected={data} onChange={date => setData(date)} className="form-control" required />
-            </Form.Group>
-            <Form.Group controlId="formServico">
-              <Form.Label>Serviço</Form.Label>
-              <Form.Control as="select" value={servico} onChange={e => setServico(e.target.value)} required>
-                <option value="" disabled>Escolha um serviço</option>
-                <option value="corte1">Corte 1</option>
-                <option value="corte2">Corte 2</option>
-                <option value="corte3">Corte 3</option>
-                <option value="barba">Barba</option>
-                <option value="bigode">Bigode</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="formNome">
-              <Form.Label>Nome</Form.Label>
-              <Form.Control type="text" value={nome} onChange={e => setNome(e.target.value)} required />
-            </Form.Group>
-            <Form.Group controlId="formTelefone">
-              <Form.Label>Telefone</Form.Label>
-              <Form.Control type="tel" value={telefone} onChange={e => setTelefone(e.target.value)} required />
-            </Form.Group>
-            <Button variant="primary" type="submit">Agendar</Button>
-          </Form>
+          <>
+          </>
         )}
-      </Modal.Body>
-    </Modal>
+        {formSubmitted ? (
+          <Alert severity="success">Serviço agendado com sucesso!</Alert>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            {activeStep === 0 && (
+              <Box>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <InputLabel
+                      sx={{
+                        textTransform: 'uppercase',
+                        color: '#000',
+                        fontWeight: 'bold',
+                      }}
+                    >A REGIÃO PARA O SERVIÇO</InputLabel>
+
+                    <TextField
+                      fullWidth
+                      margin="normal"
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="start">
+                            <LocationOnIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      value={regiao}
+                      onChange={e => setRegiao(e.target.value)}
+                      required
+
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <InputLabel
+                      sx={{
+                        textTransform: 'uppercase',
+                        color: '#000',
+                        fontWeight: 'bold',
+                        mb: 2
+                      }}
+                    >
+                      Selecione uma data
+                    </InputLabel>
+                    <DatePicker sx={{
+                      width: '100%'
+                    }}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <InputLabel
+                      sx={{
+                        textTransform: 'uppercase',
+                        color: '#000',
+                        fontWeight: 'bold',
+                      }}
+                    >
+                      Selecione um serviço
+                    </InputLabel>
+                    <FormControl fullWidth margin="normal">
+                      <InputLabel>Serviço</InputLabel>
+                      <Select
+                        value={servico}
+                        onChange={e => setServico(e.target.value)}
+                        required
+                        variant="outlined"
+                      >
+                        <MenuItem value="" disabled>Escolha um serviço</MenuItem>
+                        <MenuItem value="corte1">Corte 1</MenuItem>
+                        <MenuItem value="corte2">Corte 2</MenuItem>
+                        <MenuItem value="corte3">Corte 3</MenuItem>
+                        <MenuItem value="barba">Barba</MenuItem>
+                        <MenuItem value="bigode">Bigode</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleNext}
+                        sx={{
+                          background: '#DEC7A6',
+                          color: '#000',
+                          fontWeight: 800,
+                          borderRadius: 0,
+                          "&:hover": {
+                            background: '#DEC7A6',
+                            color: '#000',
+                            filter: 'brightness(0.9)'
+                          }
+                        }}
+                        size='large'
+                      >
+                        Continuar
+                      </Button>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+
+            )}
+            {activeStep === 1 && (
+              <Box>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <InputLabel
+                      sx={{
+                        textTransform: 'uppercase',
+                        color: '#000',
+                        fontWeight: 'bold',
+
+                      }}
+                    >
+                      Selecione o profissional
+                    </InputLabel>
+                    <FormControl fullWidth margin="normal">
+
+                      <Select
+                        value={selectedProfissional}
+                        variant="outlined"
+                        onChange={e => setSelectedProfissional(e.target.value)}
+                        required
+                      >
+                        <MenuItem value="" disabled>Escolha um profissional</MenuItem>
+                        {profissionais.map(prof => (
+                          <MenuItem key={prof.id} value={prof.id.toString()}>{prof.nome}</MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <InputLabel
+                      sx={{
+                        textTransform: 'uppercase',
+                        color: '#000',
+                        fontWeight: 'bold',
+
+                      }}
+                    >
+                      Escolha um horário
+                    </InputLabel>
+                    <TextField
+                      fullWidth
+                      margin="normal"
+
+                      variant="outlined"
+                      type="time"
+                      value={horario}
+                      onChange={e => setHorario(e.target.value)}
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      required
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={handleBack}
+                        sx={{
+                          background: '#DEC7A6',
+                          color: '#000',
+                          fontWeight: 800,
+                          borderRadius: 0,
+                          "&:hover": {
+                            background: '#DEC7A6',
+                            color: '#000',
+                            filter: 'brightness(0.9)'
+                          }
+                        }}
+                        size='large'
+                      >
+                        Voltar
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleNext}
+                        sx={{
+                          background: '#DEC7A6',
+                          color: '#000',
+                          fontWeight: 800,
+                          borderRadius: 0,
+                          "&:hover": {
+                            background: '#DEC7A6',
+                            color: '#000',
+                            filter: 'brightness(0.9)'
+                          }
+                        }}
+                        size='large'
+
+                      >
+                        Continuar
+                      </Button>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+            )}
+            {activeStep === 2 && (
+              <Box>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <InputLabel
+                      sx={{
+                        textTransform: 'uppercase',
+                        color: '#000',
+                        fontWeight: 'bold',
+
+                      }}
+                    >
+                      informe seu nome
+                    </InputLabel>
+                    <TextField
+                      fullWidth
+                      margin="normal"
+                      label="Nome"
+                      type="text"
+                      value={nome}
+                      onChange={e => setNome(e.target.value)}
+                      required
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <InputLabel
+                      sx={{
+                        textTransform: 'uppercase',
+                        color: '#000',
+                        fontWeight: 'bold',
+
+                      }}
+                    >
+                      informe seu telefone
+                    </InputLabel>
+                    <TextField
+                      fullWidth
+                      margin="normal"
+                      label="Telefone"
+                      type="tel"
+                      value={telefone}
+                      onChange={e => setTelefone(e.target.value)}
+                      required
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={handleBack}
+                        sx={{
+                          background: '#DEC7A6',
+                          color: '#000',
+                          fontWeight: 800,
+                          borderRadius: 0,
+                          "&:hover": {
+                            background: '#DEC7A6',
+                            color: '#000',
+                            filter: 'brightness(0.9)'
+                          }
+                        }}
+                        size='large'
+                      >
+                        Voltar
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        sx={{
+                          background: '#DEC7A6',
+                          color: '#000',
+                          fontWeight: 800,
+                          borderRadius: 0,
+                          "&:hover": {
+                            background: '#DEC7A6',
+                            color: '#000',
+                            filter: 'brightness(0.9)'
+                          }
+                        }}
+                        size='large'
+                      >
+                        Confirmar Reserva
+                      </Button>
+
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+            )}
+          </form>
+        )}
+      </Box>
+    </Modal >
   );
 };
 
