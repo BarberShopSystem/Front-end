c5f5fe1b7aef22a33307115d24f75517f06904bc
Limael <limagustavorafael@hotmail.com>
2024-06-13 21:35:12 -0300

    Addes post example

--- src/pages/utils/ModalRegister/index.ts
+++ src/pages/utils/ModalRegister/index.ts
@@ -1,10 +1,27 @@
 import { AxiosRequestConfig } from "axios";
 import { apiRequest } from "../../../shared/services/api";
+type Profissional = {
+    id: number;
+    nome: string;
+};
 
+type ModalProps = {
+    show: boolean;
+    handleClose: () => void;
+    profissionais: Profissional[];
+};
 export const getAlgumaCoisa = async () => {
     const config: AxiosRequestConfig = {
         url: '/alguma-rota-que-retorna',
         method: 'GET'
+    };
+    return await apiRequest<any>(config);
+};
+export const postAlgumaCoisa = async (data: ModalProps) => {
+    const config: AxiosRequestConfig = {
+        url: '/alguma-rota-que-retorna',
+        method: 'GET',
+        data
     };
     return await apiRequest<any>(config);
 };
